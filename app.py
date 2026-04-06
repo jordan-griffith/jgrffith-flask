842d7981558b311b3da127473e92f3bbbc407172
Jordan Griffith <jordan-griffith@uiowa.edu>
2026-04-05 22:24:47 -0500

    Add templates, Bootstrap, and navigation

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello world!!"
+    return render_template('index.html')
+
+@app.route("/about")
+def about_page():
+    return render_template('about.html')
 
 if __name__ == "__main__":
     app.run(debug=True, port=5002)
